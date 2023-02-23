e9b3dc3019c82d7993f219a1abf4de0f7c5f9681
Avi Bhatnagar <avi.bhatnagar2208@gmail.com>
2023-02-22 19:30:11 -0600

    added auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { TaskModule } from './task/task.module';
       'mongodb+srv://AviBhatnagar:4lic8gonBkd2Otab@cluster0.3xen19x.mongodb.net/to-do-list?retryWrites=true&w=majority',
     ),
     TaskModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
